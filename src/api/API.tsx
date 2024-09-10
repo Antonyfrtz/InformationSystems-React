0d38c4d9ae539e263dcb728c8075973ab19b8ff8
Antonis Fritzelas <antonyfrtz@gmail.com>
2024-09-10 15:58:38 +0300

    API call for singular company

--- src/api/API.tsx
+++ src/api/API.tsx
@@ -1,4 +1,4 @@
-import { CompanySearch } from "./company";
+import { CompanyProfile, CompanySearch } from "./company";
 import axios from "axios";
 
 interface SearchResponse {
@@ -8,6 +8,17 @@ interface SearchResponse {
 export const searchCompanies = async(query: string) => {
     try{
         const data = await axios.get<SearchResponse>(`https://financialmodelingprep.com/api/v3/search?query=${query}&limit=10&exchange=NASDAQ&apikey=${import.meta.env.VITE_API_KEY}`)
+        return data
+    }
+    catch(error){
+        console.log(error)
+        return error
+    }
+}
+
+export const getCompanyProfile = async(query: string) => {
+    try{
+        const data = await axios.get<CompanyProfile[]>(`https://financialmodelingprep.com/api/v3/profile/${query}?apikey=${import.meta.env.VITE_API_KEY}`)
         return data
     }
     catch(error){
--- src/pages/company/CompanyPage.tsx
+++ src/pages/company/CompanyPage.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { CompanyProfile } from '../../api/company'
+import { getCompanyProfile } from '../../api/API'
 
 interface Props {}
 
 const CompanyPage = (props: Props) => {
+
+  let {ticker} = useParams()
+  const [company, setCompany] = React.useState<CompanyProfile>()
+
+  useEffect(() => {
+    const getProfileInit = async () => {
+      const result = await getCompanyProfile(ticker!) as any;
+      setCompany(result.data[0])
+    }
+    getProfileInit()
+  },[])
+
   return (
-    <div>CompanyPage</div>
+    <>{company ? (<div>{company.companyName}</div>):(<div>Company not found!</div>)}</>
   )
 }
 
